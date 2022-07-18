b4f2b3e5463b8ca4f58144b2cf0a3981f1c43596
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-18 13:32:47 +0200

    Add test for issue 14.

---
+++ tests/issue-14/test-issie-14.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test for issue 14.
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Documentor
+ */
+
+/**
+ * Test issue 14
+ * 
+ * @ignore
+ */
+do_action( 'test_issue_14' );
