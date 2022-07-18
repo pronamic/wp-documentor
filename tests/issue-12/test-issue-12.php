210f7d6836e318db33045150a6edded555c71d09
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-18 15:18:03 +0200

    Update test-issue-12.php

--- tests/issue-12/test-issue-12.php
+++ tests/issue-12/test-issue-12.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Test for issue 14.
+ * Test for issue 12.
  *
  * @author    Pronamic <info@pronamic.eu>
  * @copyright 2005-2022 Pronamic
@@ -11,9 +11,9 @@
 /**
  * Test issue 12 A
  */
-do_action( 'test_issue_14', 'test' );
+do_action( 'test_issue_12', 'test' );
 
 /**
  * Test issue 12 B
  */
-do_action( 'test_issue_14' );
+do_action( 'test_issue_12' );
