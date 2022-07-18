aa57bde8c4a3e41a3fe0ccabf6aca8f6970e47f1
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-18 15:14:03 +0200

    Add test for pull #12.

---
+++ tests/issue-12/test-issue-12.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Test for issue 14.
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Documentor
+ */
+
+/**
+ * Test issue 12 A
+ */
+do_action( 'test_issue_14', 'test' );
+
+/**
+ * Test issue 12 B
+ */
+do_action( 'test_issue_14' );
